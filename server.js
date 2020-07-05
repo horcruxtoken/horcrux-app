753812165576516f2a85c9c20e309b56a21c24be
TheHorcruxToken <{ID}+{TheHorcruxToken}@users.noreply.github.com>
2020-07-05 12:56:01 -0400

    aws test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.port || 3000;
 
 
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
     // res.send('whats up baby')
